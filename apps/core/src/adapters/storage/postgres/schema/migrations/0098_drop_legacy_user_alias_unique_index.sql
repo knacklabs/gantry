f308af517ed426192661b894833d1dc7584d405b
Suraj-Bangade <surajbangade19@gmail.com>
2026-07-14 13:54:09 +0530

    Align identity migrations with upstream sequence

--- apps/core/src/adapters/storage/postgres/schema/migrations/0098_drop_legacy_user_alias_unique_index.sql
+++
@@ -1,5 +0,0 @@
-DROP INDEX IF EXISTS idx_user_aliases_provider_external;
-DROP INDEX IF EXISTS idx_users_app_display_name;
-
-ALTER TABLE person_merge_audit
-  ADD COLUMN IF NOT EXISTS result_json jsonb NOT NULL DEFAULT '{}'::jsonb;
--- apps/core/src/adapters/storage/postgres/schema/migrations/0100_person_merge_audit_result.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE person_merge_audit
-  ADD COLUMN IF NOT EXISTS result_json jsonb NOT NULL DEFAULT '{}'::jsonb;
